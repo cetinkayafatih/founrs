ca95ec416792d35d010eb015408eda52480d93a0
robocetai <fatihctinkya@outlook.com>
2025-09-09 15:41:59 +0300

    i18n: load all namespaces via getMessages; add metric suffixes EN/TR

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,17 +1,14 @@
 import { getRequestConfig } from 'next-intl/server'
+import { locales, type Locale } from './routing'
+import getMessages from './getMessages'
 
-import { locales } from './routing'
- 
 export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) {
-    return {
-      messages: {}
-    }
+  if (!locales.includes(locale as Locale)) {
+    return { messages: {} }
   }
- 
+
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: await getMessages(locale as Locale),
   }
 })
 
